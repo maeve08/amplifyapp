b4890f3e96a85e8491890b5c01bf0fbbd39aa37b
maeve08 <maeve08@gmail.com>
2019-10-11 16:00:09 +1100

    Adding build

---
+++ build/precache-manifest.1e2dc8effdd6467138898f551bb4f1e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d94702bd7c905aef7f06b3e8cf028768",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5db16fa30b885eb8019",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "7d367422c3eb628a9c14",
+    "url": "/static/js/2.64e8962d.chunk.js"
+  },
+  {
+    "revision": "d5db16fa30b885eb8019",
+    "url": "/static/js/main.8a64ecc5.chunk.js"
+  },
+  {
+    "revision": "fd9fcd3964e679c386c7",
+    "url": "/static/js/runtime-main.4c3bbcfa.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
